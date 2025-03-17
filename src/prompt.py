e2078abd16e1d2004950464b4fc630ad52f2923f
SarathKumar1304 <raosarathkumar@gmail.com>
2025-03-17 11:13:43 +0530

    Fix line endings to LF

---
+++ src/prompt.py
@@ -0,0 +1,14 @@
+
+
+
+
+
+system_prompt = (
+    "You are an assistant for question-answering task."
+    "Use he following pieces of retrieved context to answer"
+    "the question. If you don't know the answer , say that you don't know."
+    "Use three sentence maximum and keep the answer concise."
+    "\n\n"
+    "{context}"
+
+)
